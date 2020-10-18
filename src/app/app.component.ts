a63d69c3a4ea43da7085ad1966d7fa9f5d0f5350
manikandan <smanik@thoughtworks.com>
2020-10-18 13:42:31 +0530

    front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AppUser} from "./models/app-user.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dating-app-fe';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+  users : AppUser[];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<AppUser[]>('https://localhost:5001/api/appusers')
+      .subscribe(data => {
+        console.log(data);
+        this.users = data;
+      });
+  }
+
 }
