91be4ee814a5ca76a0000437e6c0ab3426c9e710
manikandan <smanik@thoughtworks.com>
2020-10-25 21:56:28 +0530

    css for home screen hero banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,9 @@ import {AppUser} from "./models/app-user.model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'Dating App';
-  users : AppUser[];
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<AppUser[]>('https://localhost:5001/api/appusers')
-      .subscribe(data => {
-        console.log(data);
-        this.users = data;
-      });
   }
 
 }
