cb2ef9f269ce5075cb0ecbc2975ea1f7a1b7ebbf
manikandan <smanik@thoughtworks.com>
2020-12-19 21:58:55 +0530

    image grid with hover animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProfileComponent} from './components/profile/profile.component';
+import {JobsComponent} from "./components/jobs/jobs.component";
 
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent}
+  { path: 'profile', component: ProfileComponent},
+  { path: 'jobs', component: JobsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { JobsComponent } from './components/jobs/jobs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
-    ProfileComponent
+    ProfileComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule,
